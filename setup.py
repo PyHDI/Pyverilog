c84a0a086f29ec78570482be6323c4befe896381
shtaxxx <shta.ky1018@gmail.com>
2014-11-10 19:47:15 +0900

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setuptools import setup, find_packages
 
 import pyverilog.utils.version
 import re
+import os
 
 m = re.search(r'(\d+\.\d+\.\d+)', pyverilog.utils.version.VERSION)
 version = m.group(1) if m is not None else '0.0.0'
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 setup(name='pyverilog',
       version=version,
@@ -12,6 +16,7 @@ setup(name='pyverilog',
       long_description=read('README.md'),
       keywords = 'Verilog HDL, Lexer, Parser, Dataflow Analyzer, Control-flow Analyzer, Code Generator, Visualizer',
       author='Shinya Takamaeda-Yamazaki',
+      author_email='shinya.takamaeda_at_gmail_com',
       license="Apache License 2.0",
       url='http://shtaxxx.github.io/Pyverilog/',
       packages=find_packages(),
