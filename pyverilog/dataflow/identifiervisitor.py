360457a6ea9637f7df6ea47249dcc23d50cc4910
shtaxxx <shta.ky1018@gmail.com>
2015-05-29 19:26:07 +0900

    identifiervisitor.py: returns an identifier list in a nested AST.

---
+++ pyverilog/dataflow/identifiervisitor.py
@@ -0,0 +1,44 @@
+#-------------------------------------------------------------------------------
+# identifiervisitor.py
+# 
+# Identifier list generator in a nested operator 
+#
+# Copyright (C) 2015, Shinya Takamaeda-Yamazaki
+# License: Apache 2.0
+#-------------------------------------------------------------------------------
+
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) )
+
+if sys.version_info[0] >= 3:
+    from pyverilog.dataflow.visit import NodeVisitor
+else:
+    from visit import NodeVisitor
+
+class IdentifierVisitor(NodeVisitor):
+    def __init__(self):
+        self.identifiers = []
+
+    def getIdentifiers(self):
+        return tuple(self.identifiers)
+
+    def reset(self):
+        self.identifiers = []
+    
+    def visit_Identifier(self, node):
+        self.identifiers.append(node.name)
+
+if __name__ == '__main__':
+    import pyverilog.vparser.ast as vast
+
+    a = vast.Identifier('a')
+    b = vast.Identifier('b')
+    c = vast.Plus(a, b)
+
+    v = IdentifierVisitor()
+    v.visit(c)
+    
+    ids = v.getIdentifiers()
+    print(ids)
