b945284ac77a93cd0fcd63396e83526932a23741
shtaxxx <shta.ky1018@gmail.com>
2015-08-20 17:53:46 +0900

    absolute_import and print_function are used in ast_code_generator

--- pyverilog/ast_code_generator/sample/led.py
+++ pyverilog/ast_code_generator/sample/led.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+from __future__ import print_function
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))) )
