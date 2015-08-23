32698babacf4853cfe1879b6017d2b2bf3e31439
shtaxxx <shta.ky1018@gmail.com>
2015-08-24 02:16:03 +0900

    Pytest modules of test_case_in_func and test_func are updated: Printing order and test name

--- tests/dataflow_test/test_case_in_func.py
+++ tests/dataflow_test/test_case_in_func.py
@@ -1,20 +1,22 @@
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) )
+
 from pyverilog.dataflow.dataflow_analyzer import VerilogDataflowAnalyzer
 from pyverilog.dataflow.optimizer import VerilogDataflowOptimizer
 from pyverilog.controlflow.controlflow_analyzer import VerilogControlflowAnalyzer
+
 codedir = '../../testcode/'
 
 expected = """\
-TOP.SEL: TOP_SEL
-TOP.md_always0.al_block0.al_functioncall0.in1: TOP_IN1
-TOP.md_always0.al_block0.al_functioncall0._rn1_func1: 1'd0
-TOP.bit: (((TOP_SEL=='d0))? TOP_IN1 : 1'd0)
-TOP.md_always0.al_block0.al_functioncall0.func1: (((TOP_SEL=='d0))? TOP_IN1 : 1'd0)
-TOP.md_always0.al_block0.al_functioncall0.sel: TOP_SEL
-TOP.md_always0.al_block0.al_functioncall0._rn0_func1: TOP_IN1
 TOP.IN1: TOP_IN1
+TOP.SEL: TOP_SEL
+TOP.bit: (((TOP_SEL=='d0))? TOP_IN1 : 1'd0)
+TOP.md_always0.al_block0.al_functioncall0._rn0_func1: TOP_IN1
+TOP.md_always0.al_block0.al_functioncall0._rn1_func1: 1'd0
+TOP.md_always0.al_block0.al_functioncall0.func1: (((TOP_SEL=='d0))? TOP_IN1 : 1'd0)
+TOP.md_always0.al_block0.al_functioncall0.in1: TOP_IN1
+TOP.md_always0.al_block0.al_functioncall0.sel: TOP_SEL
 """
 
 def test():
@@ -48,14 +50,15 @@ def test():
                                             )
 
     output = []
-    for tk in sorted(c_analyzer.resolved_terms.keys(), key=lambda x:str(x[0])):
+    for tk in sorted(c_analyzer.resolved_terms.keys(), key=lambda x:str(x)):
         tree = c_analyzer.makeTree(tk)
         output.append(str(tk) + ': ' + tree.tocode())
 
     rslt = '\n'.join(output) + '\n'
 
     print(rslt)
-    assert(rslt == expected)
+    
+    assert(expected == rslt)
 
 if __name__ == '__main__':
     test()
--- tests/dataflow_test/test_function.py
+++ tests/dataflow_test/test_function.py
@@ -1,19 +1,21 @@
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) )
+
 from pyverilog.dataflow.dataflow_analyzer import VerilogDataflowAnalyzer
 from pyverilog.dataflow.optimizer import VerilogDataflowOptimizer
 from pyverilog.controlflow.controlflow_analyzer import VerilogControlflowAnalyzer
+
 codedir = '../../testcode/'
 
 expected = """\
-TOP.RST_X: TOP_RST_X
-TOP.md_always0.al_block0.al_if0_ELSE.al_block2.al_functioncall0.inc: (((!TOP_RST_X))? TOP_al_block0_al_block2_al_functioncall0_inc : (((!TOP_RST_X))? (((&TOP_al_block0_al_block2_al_functioncall0_inc))? 'd0 : (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1))) : (((&TOP_al_block0_al_block2_al_functioncall0_inc))? (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1)) : (((!TOP_RST_X))? (((&(TOP_al_block0_al_block2_al_functioncall0_inc+'d1)))? 'd0 : (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1))) : (((&(TOP_cnt+'d1)))? 'd0 : (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1)))))))
-TOP.md_always0.al_block0.al_if0_ELSE.al_block2.al_functioncall0._rn1_inc: (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0__rn1_inc+'d1) : (TOP_cnt+'d1))
-TOP.cnt: (((!TOP_RST_X))? 'd0 : (((!TOP_RST_X))? TOP_cnt : (((&TOP_al_block0_al_block2_al_functioncall0_inc))? 'd0 : (((!TOP_RST_X))? (TOP_cnt+'d1) : (TOP_cnt+'d1)))))
-TOP.md_always0.al_block0.al_if0_ELSE.al_block2.al_functioncall0.in: (((!TOP_RST_X))? TOP_al_block0_al_block2_al_functioncall0_in : TOP_cnt)
 TOP.CLK: TOP_CLK
+TOP.RST_X: TOP_RST_X
+TOP.cnt: (((!TOP_RST_X))? 'd0 : (((!TOP_RST_X))? TOP_cnt : (((&TOP_al_block0_al_block2_al_functioncall0_inc))? 'd0 : (((!TOP_RST_X))? (TOP_cnt+'d1) : (TOP_cnt+'d1)))))
 TOP.md_always0.al_block0.al_if0_ELSE.al_block2.al_functioncall0._rn0_inc: 'd0
+TOP.md_always0.al_block0.al_if0_ELSE.al_block2.al_functioncall0._rn1_inc: (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0__rn1_inc+'d1) : (TOP_cnt+'d1))
+TOP.md_always0.al_block0.al_if0_ELSE.al_block2.al_functioncall0.in: (((!TOP_RST_X))? TOP_al_block0_al_block2_al_functioncall0_in : TOP_cnt)
+TOP.md_always0.al_block0.al_if0_ELSE.al_block2.al_functioncall0.inc: (((!TOP_RST_X))? TOP_al_block0_al_block2_al_functioncall0_inc : (((!TOP_RST_X))? (((&TOP_al_block0_al_block2_al_functioncall0_inc))? 'd0 : (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1))) : (((&TOP_al_block0_al_block2_al_functioncall0_inc))? (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1)) : (((!TOP_RST_X))? (((&(TOP_al_block0_al_block2_al_functioncall0_inc+'d1)))? 'd0 : (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1))) : (((&(TOP_cnt+'d1)))? 'd0 : (((!TOP_RST_X))? (TOP_al_block0_al_block2_al_functioncall0_inc+'d1) : (TOP_cnt+'d1)))))))
 """
 
 def test():
@@ -47,14 +49,14 @@ def test():
                                             )
 
     output = []
-    for tk in sorted(c_analyzer.resolved_terms.keys(), key=lambda x:str(x[0])):
+    for tk in sorted(c_analyzer.resolved_terms.keys(), key=lambda x:str(x)):
         tree = c_analyzer.makeTree(tk)
         output.append(str(tk) + ': ' + tree.tocode())
 
     rslt = '\n'.join(output) + '\n'
 
     print(rslt)
-    assert(rslt == expected)
+    assert(expected == rslt)
 
 if __name__ == '__main__':
     test()
