15e2a32a4fdb7c7c14a0a4fd5cbd509cf1b3b6c9
shtaxxx <shta.ky1018@gmail.com>
2017-10-01 11:58:10 +0900

    test script

--- scripts/test-python2.sh
+++ scripts/test-python2.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 set -e
 
-mkdir 3.5
-cd 3.5
-virtualenv --python=python3 .
+mkdir python
+cd python
+virtualenv --python=python .
 source bin/activate
+
 git clone https://github.com/PyHDI/Pyverilog.git
 cd Pyverilog
-python3 setup.py install
+python setup.py install
 pip install pytest pytest-pythonpath
 mv pyverilog pyverilog.old
-make -C examples
-make clean -C examples
-make test -C tests
+
+python -m pytest -vv .
+
 mv pyverilog.old pyverilog
 cd ..
 deactivate
--- scripts/test-python3.sh
+++ scripts/test-python3.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 set -e
 
-mkdir 2.7
-cd 2.7
-virtualenv --python=python .
+mkdir python3
+cd python3
+virtualenv --python=python3 .
 source bin/activate
+
 git clone https://github.com/PyHDI/Pyverilog.git
 cd Pyverilog
 python setup.py install
 pip install pytest pytest-pythonpath
 mv pyverilog pyverilog.old
-make -C examples PYTHON=python
-make clean -C examples
-make test -C tests PYTHON=python
+
+python -m pytest -vv .
+
 mv pyverilog.old pyverilog
 cd ..
 deactivate
