7c38313f41aac8db2a168b6c9da0d5b2a5c44707
shtaxxx <shta.ky1018@gmail.com>
2017-10-07 01:54:45 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -7,22 +7,24 @@ import os
 m = re.search(r'(\d+\.\d+\.\d+(-.+)?)', pyverilog.utils.version.VERSION)
 version = m.group(1) if m is not None else '0.0.0'
 
+
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
 
 setup(name='pyverilog',
       version=version,
       description='Python-based Hardware Design Processing Toolkit for Verilog HDL: Parser, Dataflow Analyzer, Controlflow Analyzer and Code Generator',
       long_description=read('README.rst'),
-      keywords = 'Verilog HDL, Lexer, Parser, Dataflow Analyzer, Control-flow Analyzer, Code Generator, Visualizer',
+      keywords='Verilog HDL, Lexer, Parser, Dataflow Analyzer, Control-flow Analyzer, Code Generator, Visualizer',
       author='Shinya Takamaeda-Yamazaki',
       license="Apache License 2.0",
       url='https://github.com/PyHDI/Pyverilog',
       packages=find_packages(),
-      package_data={ 'pyverilog.ast_code_generator' : ['template/*'],  },
-      install_requires=[ 'Jinja2>=2.8' ],
+      package_data={'pyverilog.ast_code_generator': ['template/*'], },
+      install_requires=['Jinja2>=2.8'],
       extras_require={
-          'graph' : [ 'pygraphviz>=1.3.1' ],
-          'test' : [ 'pytest>=2.8.2', 'pytest-pythonpath>=0.7' ],
+          'graph': ['pygraphviz>=1.3.1'],
+          'test': ['pytest>=2.8.2', 'pytest-pythonpath>=0.7'],
       },
-)
+      )
